520856fc07cb1235448fa4dcdc5a5a1610344ecb
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-06-08 11:34:03 +0200

    Ensure we backup the database before doing migration
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- tests/test_db_migration.py
+++ tests/test_db_migration.py
@@ -1,6 +1,7 @@
 import unittest
 import tscat.orm_sqlalchemy
 import os
+from glob import glob
 import tscat
 import datetime as dt
 
@@ -16,6 +17,7 @@ class DBMigration(unittest.TestCase):
     def test_existing_event_now_has_rating_field(self):
         existing, = tscat.get_events()
         self.assertEqual(existing.rating, None)
+        self.assertGreater(len(glob(f'{tscat.base.backend()._tmp_dir}/*.sqlite.backup')), 0)
 
     def test_creating_event_with_rating(self):
         tscat.create_event(dt.datetime.now(), dt.datetime.now() + dt.timedelta(days=1), "Patrick", rating=3)
@@ -25,3 +27,12 @@ class DBMigration(unittest.TestCase):
         self.assertEqual(e.rating, 3)
 
         self.assertEqual(len(tscat.get_events()), 2)
+
+
+class DBMigrationBackup(unittest.TestCase):
+
+    def setUp(self):
+        if tscat.base._backend:
+            tscat.base._backend.close()
+        test_db_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'migration-db-test.sqlite')
+        tscat.base._backend = tscat.orm_sqlalchemy.Backend(testing=test_db_file)
